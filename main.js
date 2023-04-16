2a36a57b618800bbf1d3a12c72e1eff0ad00ea20
Breno Neves <75847432+Brenoajz@users.noreply.github.com>
2023-04-16 08:43:55 -0300

    Adicionando funcionalidades.
    Usuário consegue fazer cadastro e login.
    Dados armazenados em uma lista dentro do código. É armazenada apenas quando o código está rodando, quando a atualizado as informações somem.

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 function cadastro() {
         document.querySelector("#box").innerHTML = `
 <div id="main">
-    <h1 class="text">Cadrastre-se</h1>
-    <input type="text" placeholder="Nome e Sobrenome">
+    <h1 class="text">Cadastre-se</h1>
+    <input type="text" placeholder="Nome" id="Nome">
     <br><br>
-    <input type="text" placeholder="E-mail">
+    <input type="password" placeholder="Senha" maxlength="10" id="Senha">
     <br><br>
-    <input type="password" placeholder="Senha" maxlength="10">
-    <br><br>
-    <button type="submit">Cadrastre-se</button>
+    <button type="submit" onclick="cad()">Cadastre-se</button>
     <br><br>
     <p class="sec-text">Já tem uma conta? Faça o <a onclick="login()">login!</a></p>
 </div>
@@ -18,16 +16,53 @@ function login() {
         document.querySelector("#box").innerHTML = `
     <div id="main">
         <h1 class="text">Login</h1>
-        <input type="text" placeholder="E-mail">
+        <input type="text" placeholder="Nome" id="Nome">
         <br><br>
-        <input type="password" placeholder="Senha" maxlength="10">
+        <input type="password" placeholder="Senha" maxlength="10" id="Senha">
         <br><br>
-        <button type="submit">Login</button>
+        <button type="submit" onclick="log()">Login</button>
         <br><br>
         <p class="sec-text">Faça o login. Ou crie uma <a onclick="cadastro()">conta!</a></p>
     </div>
     `;
 }
 
-  
+let usuarios = [];
 
+function cad() {
+
+  const nome = document.getElementById("Nome").value;
+  const senha = document.getElementById("Senha").value;
+
+  if (nome.trim() === "") {
+    alert("Por favor, insira um nome válido");
+    return;
+  }
+
+  if (senha.trim() === "" || senha.length < 6) {
+    alert("Por favor, insira uma senha válida (pelo menos 6 caracteres)");
+    return;
+  }
+
+  usuarios.push({ nome: nome, senha: senha });
+
+  document.getElementById("Nome").value = "";
+  document.getElementById("Senha").value = "";
+
+  alert("Usuário cadastrado com sucesso!");
+}
+
+function log() {
+  const nome = document.getElementById("Nome").value;
+  const senha = document.getElementById("Senha").value;
+
+  const usuarioIndex = usuarios.findIndex(
+    (usuario) => usuario.nome === nome && usuario.senha === senha
+  );
+
+  if (usuarioIndex !== -1) {
+    alert("Bem vindo!");
+  } else {
+    alert("Dados incorretos, se não tem uma conta, crie.");
+  }
+}
